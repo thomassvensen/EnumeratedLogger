8754971948886cd0db66b8b2459f7e3902d8dc11
Thomas Svensen <thomas.svensen@bekk.no>
2014-08-11 13:05:18 +0200

    Improved NuGet build process - automatically building .nupkg to /build on every build

--- src/EnumLogger/Properties/AssemblyInfo.cs
+++ src/EnumLogger/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("EnumLogger")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EnumLogger")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,15 +10,15 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0ea4f583-dd8f-4ab7-a7d0-b1121934764c")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("Enumerated Logger")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyTitle("EnumeratedLogger")]
+[assembly: AssemblyDescription("An add-on on top of Log4Net that structures logging via enums.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Thomas Svensen")]
+[assembly: AssemblyCopyright(" Copyright © 2014")]
+
+
